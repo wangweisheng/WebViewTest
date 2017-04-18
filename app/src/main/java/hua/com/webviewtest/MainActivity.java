d87fe0fc4ee15ed48409bb907d486624d75ae869
Hua <1057918521@qq.com>
2017-04-18 17:03:19 +0800

    Initial commit

---
+++ app/src/main/java/hua/com/webviewtest/MainActivity.java
@@ -0,0 +1,49 @@
+package hua.com.webviewtest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn_go;
+    private EditText et_url;
+    private String urlHead="http://";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+
+
+    private void initView() {
+        btn_go= (Button) findViewById(R.id.btn_go);
+        et_url= (EditText) findViewById(R.id.et_url);
+        btn_go.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_go:
+               Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(urlHead+et_url.getText().toString()));
+                Intent choose=Intent.createChooser(intent,"选择一个浏览器");
+                startActivity(choose);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/hua/com/webviewtest/MyWebView.java
@@ -0,0 +1,51 @@
+package hua.com.webviewtest;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by qinghua on 2017/4/18.
+ */
+
+public class MyWebView extends AppCompatActivity {
+
+    private WebView webView;
+    private String url;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myview_layout);
+        initView();
+        setWebView();
+    }
+
+    private void initView() {
+        webView= (WebView) findViewById(R.id.webView);
+        url=getIntent().getData().toString();
+    }
+
+
+    private void setWebView() {
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(true);
+
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
